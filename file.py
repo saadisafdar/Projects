0aaceb1b77c70610c7ec643850ebff89bdd5d71a
Saadi-Safdar-Pro <saadisafder@gmail.com>
2025-12-01 19:54:43 +0500

    Resettuped the virtual environment

---
+++ file.py
@@ -0,0 +1,23 @@
+# import pandas as pd
+
+# df = pd.read_csv("students.csv")
+
+# print(df)
+
+
+
+# import seaborn as sns
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# # Create DataFrame from dataset
+# data = {'Scores': [45, 56, 67, 70, 90, 85, 88, 77, 50, 95]}
+# df = pd.DataFrame(data)
+# # Create histogram
+# sns.histplot(data=df, x='Scores', bins=5, color='purple')
+# plt.title('Distribution of coding Test Scores')
+# plt.xlabel('Scores')
+# plt.ylabel('Frequency')
+# plt.show()
+
+
