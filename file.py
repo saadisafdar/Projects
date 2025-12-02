1429fb9e73fabe0c617cfb35fde57ba51fceee26
Saadi-Safdar-Pro <saadisafder@gmail.com>
2025-12-02 10:26:45 +0500

    Gave Lab Sessional

--- file.py
+++ file.py
@@ -1,23 +1,24 @@
-# import pandas as pd
+import pandas as pd
 
-# df = pd.read_csv("students.csv")
+df = pd.read_csv("patients.csv")
 
-# print(df)
+print(df)
+    
+high_health = df[df['health_index'] > 92]
 
+high_cost = df[df['treatment_cost'] > 100000]
 
+print("Patients with highest health index:\n ", high_health['ID'], high_health['health_index'])
 
-# import seaborn as sns
-# import pandas as pd
-# import matplotlib.pyplot as plt
+print("Patients with highest treatment cost:\n ",high_cost['ID'])
+ 
+import numpy as np
 
-# # Create DataFrame from dataset
-# data = {'Scores': [45, 56, 67, 70, 90, 85, 88, 77, 50, 95]}
-# df = pd.DataFrame(data)
-# # Create histogram
-# sns.histplot(data=df, x='Scores', bins=5, color='purple')
-# plt.title('Distribution of coding Test Scores')
-# plt.xlabel('Scores')
-# plt.ylabel('Frequency')
-# plt.show()
+heartbeat_readings = [72, 78, 75, 82, 88, 85, 92]
+
+difference = np.diff(heartbeat_readings)
 
 
+print("Name: ", " Saadi Safdar, ", " DOB: ", "4/2/2006")
+print("Father name: ", " Muhammad Safdar Butt, ", " DOB: ", "31/8/1978")
+print("Name: ", " Muhammad Siddique Butt, ", " DOB: ", "18/5/1960")
